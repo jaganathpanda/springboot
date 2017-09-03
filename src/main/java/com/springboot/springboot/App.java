4199682273b179976a5e0d2410a27d52c0a4e97a
jaganath <jaganath.panda@yahoo.com>
2017-09-03 17:00:25 +0530

    war file

--- src/main/java/com/springboot/springboot/App.java
+++ src/main/java/com/springboot/springboot/App.java
@@ -2,12 +2,17 @@ package com.springboot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-       SpringApplication.run(App.class, args); 
-    }
+public class App extends SpringBootServletInitializer {
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(App.class);
+	    }
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
